d7dffe1beddec6062fb3325c526d23d4d376e6a3
uminocelo <marcelo.mdl.dev@gmail.com>
2026-07-12 21:54:19 -0300

    chore: add get feature

--- lib/buraco/server.ex
+++ lib/buraco/server.ex
@@ -16,8 +16,25 @@ defmodule Buraco.Server do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
+  @doc """
+  Returns the value associated with `key`.
+
+  Returns `nil` when the key does not exist.
+  """
+  @spec get(term()) :: term() | nil
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
   @impl true
   def init(_opts) do
     {:ok, %{}}
+  end
+
+  @impl true
+  def handle_call({:get, key}, _from, state) do
+    value = Map.get(state, key)
+
+    {:reply, value, state}
   end
 end
